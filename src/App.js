4acc124158c4e2bb57517a3233d681bc5391f133
v_bonchevski <v.bonchevski@valteodigital.com>
2019-10-01 15:14:55 +0300

    finally commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Component, Fragment} from 'react';
+import Home from './Home';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (<Fragment><Home/> , <h1 key="heading">Is where the heart is ?</h1></Fragment>)
+  }
+}
+export default App;
